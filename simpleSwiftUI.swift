d8d4d25ea0e454831443f50bb36cac8137f91718
PODHUMANI M <156355216+PODHUMANI@users.noreply.github.com>
2025-06-29 14:44:38 +0530

    Create simpleSwiftUI.swift

---
+++ simpleSwiftUI.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State private var inputText = ""
+    @State private var showText = ""
+
+    var body: some View {
+        VStack(spacing: 20) {
+            
+            Text(" \(showText)")
+                .font(.title2)
+                .foregroundColor(.green)
+
+           
+            
+            TextField("Enter something...", text: $inputText)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+
+            Button("Show Text") {
+                showText = inputText
+            }
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+
+            Spacer()
+        }
+        .padding()
+    }
+}
