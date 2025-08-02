e5cf4ef735ad494454e3fc3d3f8bf487ab1376ba
PODHUMANI M <156355216+PODHUMANI@users.noreply.github.com>
2025-08-02 21:34:11 +0530

    Create Button.swift

---
+++ Button.swift
@@ -0,0 +1,9 @@
+Button("Add") {
+    let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
+    let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
+
+    let chosenFirstName = firstNames.randomElement()!
+    let chosenLastName = lastNames.randomElement()!
+
+    // more code to come        
+}
