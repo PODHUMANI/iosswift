a53c6520e9c0b3a4b4e29ee8ffcc8539669d5a25
PODHUMANI M <156355216+PODHUMANI@users.noreply.github.com>
2025-10-20 20:25:49 +0530

    Create Text.swift

---
+++ Text.swift
@@ -0,0 +1,7 @@
+let label = UILabel()
+label.text = "Hello, UIKit!"
+label.textColor = .black
+label.textAlignment = .center
+label.frame = CGRect(x: 50, y: 100, width: 200, height: 40)
+
+view.addSubview(label)
