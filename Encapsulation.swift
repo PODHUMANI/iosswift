5ef6c79d4b33964e5afaf3f79e867c2dd6a8b2bf
PODHUMANI M <156355216+PODHUMANI@users.noreply.github.com>
2025-11-14 20:43:02 +0530

    Create Encapsulation.swift

---
+++ Encapsulation.swift
@@ -0,0 +1,11 @@
+class Person {
+    private String name;   // hidden data
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
