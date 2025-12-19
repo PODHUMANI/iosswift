e664ee7bffccc3fa53bcddd372e5b767d57f2ffc
PODHUMANI M <156355216+PODHUMANI@users.noreply.github.com>
2025-12-19 21:39:31 +0530

    Create NumberPalindrome.swift

---
+++ NumberPalindrome.swift
@@ -0,0 +1,17 @@
+class Solution {
+    func isPalindrome(_ x: Int) -> Bool {
+        if x < 0 || (x % 10 == 0 && x != 0) {
+            return false
+        }
+        
+        var num = x
+        var reversed = 0
+        
+        while num > reversed {
+            reversed = reversed * 10 + num % 10
+            num /= 10
+        }
+        
+        return num == reversed || num == reversed / 10
+    }
+}
