e8f0a6d03e1ea2a96b2be806f0941ff70c786941
PODHUMANI M <156355216+PODHUMANI@users.noreply.github.com>
2026-01-23 21:35:54 +0530

    Create MajorityElement.swift

---
+++ MajorityElement.swift
@@ -0,0 +1,14 @@
+class Solution {
+    func majorityElement(_ nums: [Int]) -> Int {
+        var count = 0
+        var candidate = 0
+
+        for num in nums {
+            if count == 0 {
+                candidate = num
+            }
+            count += (num == candidate) ? 1 : -1
+        }
+        return candidate
+    }
+}
