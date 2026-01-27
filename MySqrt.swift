a50759311eb990428d282824f439f66e13c417a1
PODHUMANI M <156355216+PODHUMANI@users.noreply.github.com>
2026-01-27 22:27:53 +0530

    Create MySqrt.swift

---
+++ MySqrt.swift
@@ -0,0 +1,24 @@
+class Solution {
+    func mySqrt(_ x: Int) -> Int {
+        if x < 2 {
+            return x
+        }
+
+        var left = 1
+        var right = x / 2
+        var ans = 0
+
+        while left <= right {
+            let mid = left + (right - left) / 2
+
+            if mid <= x / mid {
+                ans = mid
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
+
+        return ans
+    }
+}
