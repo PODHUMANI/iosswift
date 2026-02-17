6918d9ae472ba90a30511e2ed9bbcc24defd8e50
PODHUMANI M <156355216+PODHUMANI@users.noreply.github.com>
2026-02-17 21:57:57 +0530

    Create MaxProfit.swift

---
+++ MaxProfit.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func maxProfit(_ prices: [Int]) -> Int {
+        var minPrice = Int.max
+        var maxProfit = 0
+        
+        for price in prices {
+            if price < minPrice {
+                minPrice = price
+            } else {
+                let profit = price - minPrice
+                maxProfit = max(maxProfit, profit)
+            }
+        }
+        
+        return maxProfit
+    }
+}
+
